6698ebe8e051b23f21854207fda719efd52213e7
Shota Miyahira <2525.natuta@gmail.com>
2022-04-10 05:22:22 +0900

    footerにリンク追加

--- app/components/Layout/Footer/Footer.tsx
+++ app/components/Layout/Footer/Footer.tsx
@@ -1,14 +1,23 @@
-import { Container, Link, Text } from "@chakra-ui/react";
+import { Container, Flex, Link, Spacer, Text } from "@chakra-ui/react";
 
 export const Footer: React.FC = () => {
   return (
     <Container maxW="container.lg" mt={10}>
-      <Text>
-        © 2022; Built by
-        <Link href="https://twitter.com/shota1995m" isExternal ml={2}>
-          @shota1995m
+      <Flex>
+        <Text>
+          © 2022; Built by
+          <Link href="https://twitter.com/shota1995m" isExternal ml={2}>
+            @shota1995m
+          </Link>
+        </Text>
+        <Spacer />
+        <Link
+          href="https://github.com/shota1995m/planning-poker-remix"
+          isExternal
+        >
+          GitHub
         </Link>
-      </Text>
+      </Flex>
     </Container>
   );
 };
